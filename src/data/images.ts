56b11950e5032d8360f9e1939ac72737ba49755d
mayursuthar02 <mickymec00@gmail.com>
2025-08-24 19:07:11 +0530

    Add images

--- src/data/images.ts
+++ src/data/images.ts
@@ -3,11 +3,13 @@ import { Artwork } from "../types/artwork";
 // Import all images from two folders
 const xeionusImages = import.meta.glob("../assets/Xeionus/*.{jpg,png,webp}", { eager: true });
 const shadowArtImages = import.meta.glob("../assets/ShadowArt/*.{jpg,png,webp}", { eager: true });
+const AkatsukiImages = import.meta.glob("../assets/Akatsuki/*.{jpg,png,webp}", { eager: true });
 
 // Merge both folder images into one array
 const rawImages: string[] = [
   ...Object.values(xeionusImages).map((img: any) => img.default),
   ...Object.values(shadowArtImages).map((img: any) => img.default),
+  ...Object.values(AkatsukiImages).map((img: any) => img.default),
 ];
 
 // Finally create artworks array with id + imageUrl
